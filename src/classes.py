b21f7101bc9f5ddbed17ef363dfd61e4a9258075
Makarov.F <fedormakarov634@gmail.com>
2025-05-16 15:49:04 +0300

    init hw 17.2

--- src/classes.py
+++ src/classes.py
@@ -1,25 +1,59 @@
-class Product:
-    name: str
-    description: str
-    price: float
-    quantity: int
+from abc import ABC, abstractmethod
 
+
+class BaseProduct(ABC):  # pragma: no cover
+
+    @abstractmethod
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
         self.__price = price
         self.quantity = quantity
 
+    @abstractmethod
+    def new_product(self):
+        pass
+
+    @abstractmethod
+    def __str__(self):
+        pass
+
+    @abstractmethod
+    def __add__(self, other):
+        pass
+
+
+class MixinClass:
+
+    def __repr__(self):
+        self.product_log()
+
+    def product_log(self):
+        print(f"{self.__class__.__name__}({self.name}, {self.description}, {self.price}, {self.quantity})")
+
+
+class Product(MixinClass, BaseProduct):
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.__price = price
+        if quantity <= 0:
+            raise ValueError("Товар с нулевым количеством не может быть добавлен")
+        else:
+            self.quantity = quantity
+        super().__repr__()
+
     def __str__(self):
         return f"{self.name}, {self.__price} руб. Остаток: {self.quantity} шт."
 
     def __add__(self, other):
-        if type(self) == type(other):
+
+        if isinstance(self, type(other)):
             sum1 = self.price * self.quantity
             sum2 = other.price * other.quantity
             return sum1 + sum2
         else:
-            raise TypeError("Нельзя складывать объекты разных классов")
+            raise TypeError
 
     @classmethod
     def new_product(cls, params: dict):
@@ -47,9 +81,6 @@ class Product:
 
 
 class Category:
-    name: str
-    description: str
-    products: list
     product_count = 0
     category_count = 0
 
@@ -69,7 +100,7 @@ class Category:
         return f"{self.name}, количество продуктов: {summ} шт."
 
     def add_product(self, product):
-        if not isinstance(product, Product):
+        if not isinstance(product, Product) or not issubclass(type(product), Product):
             raise TypeError("Нельзя добавлять объекты не типа Класс")
         else:
             self.__products.append(product)
@@ -81,6 +112,17 @@ class Category:
         for i in self.__products:
             my_list.append(f"{i.name}, {i.price} руб. Остаток: {i.quantity} шт.\n")
         return my_list
+
+    def middle_price(self):
+        try:
+            prod_count = len(self.__products)
+            summ = 0
+            for i in self.__products:
+                summ += i.price
+
+            return summ / prod_count
+        except:
+            return 0
 
 
 class Smartphone(Product):
--- tests/test_classes.py
+++ tests/test_classes.py
@@ -35,6 +35,8 @@ def test_init_product(product):
     assert product.description == "512GB, Gray space"
     assert product.price == 250000.0
     assert product.quantity == 7
+    with pytest.raises(ValueError):
+        product1 = Product("Бракованный товар", "Неверное количество", 1000.0, 0)
 
 
 def test_init_category(category):
@@ -63,13 +65,16 @@ def test_product_add_new():
     assert new_product.price == 180000.0
 
 
+@patch("src.classes.MixinClass.product_log", return_value="")
 @patch("builtins.input")
-def test_product_price_set(mock_input, capsys):
+def test_product_price_set(mock_input, mock_log, capsys):
     product = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
     assert product.price == 210000.0
+    mock_input.return_value = "y"
     product.price = -100
     captured = capsys.readouterr()
     assert captured.out == "Цена не должна быть нулевая или отрицательная\n"
+    assert product.price == 210000.0
     mock_input.return_value = "y"
     product.price = 1000
     assert product.price == 1000
@@ -78,7 +83,8 @@ def test_product_price_set(mock_input, capsys):
     assert product.price == 1000
 
 
-def test_classes_methods(capsys):
+@patch("src.classes.MixinClass.product_log", return_value="")
+def test_classes_methods(mock_log, capsys):
     product1 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
     product2 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
     category = Category(
@@ -130,3 +136,16 @@ def test_subclasses_product():
     assert grass1.country == "Россия"
     assert grass1.germination_period == "7 дней"
     assert grass1.color == "Зеленый"
+
+
+def test_middle_price_category():
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 7)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 10)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 6)
+
+    category1 = Category("Смартфоны", "Категория смартфонов", [product1, product2, product3])
+
+    assert category1.middle_price() == 140333.33333333334
+
+    category_empty = Category("Смартфоны", "Категория смартфонов", [])
+    assert category_empty.middle_price() == 0
